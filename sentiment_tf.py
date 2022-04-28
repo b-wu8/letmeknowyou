7633fba78fbae977d83553efd1dcb586bf18e46b
Harry Chih <yanjie@ucsb.edu>
2022-04-27 23:52:50 -0400

    Add files via upload

--- sentiment_tf.py
+++ sentiment_tf.py
@@ -1,3 +1,4 @@
+from multiprocessing.dummy import active_children
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -9,10 +10,10 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
 if __name__ == "__main__":
-    batch_size = 512
+    batch_size = 100
     img_height = 48
     img_width = 48
-    epochs = 10
+    epochs = 60
 
     print(tf.__version__)
     print(tf.config.list_physical_devices())
@@ -25,71 +26,119 @@ if __name__ == "__main__":
     VAL_DIR = DATA_DIR + "val/"
     MODEL_DIR = "models/"
 
-    tf.debugging.set_log_device_placement(True)
+    # tf.debugging.set_log_device_placement(True)
 
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    if gpus:
-        try:
-            # Currently, memory growth needs to be the same across GPUs
-            for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
-                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-                print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            # Memory growth must be set before GPUs have been initialized
-            print(e)
+    # gpus = tf.config.experimental.list_physical_devices('GPU')
+    # if gpus:
+    #     try:
+    #         # Currently, memory growth needs to be the same across GPUs
+    #         for gpu in gpus:
+    #             tf.config.experimental.set_memory_growth(gpu, True)
+    #             logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    #             print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    #     except RuntimeError as e:
+    #         # Memory growth must be set before GPUs have been initialized
+    #         print(e)
 
 
-    train_ds = tf.keras.utils.image_dataset_from_directory(
-    TRAIN_DIR,
-    validation_split=0.2,
-    subset="training",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
 
-    val_ds = tf.keras.utils.image_dataset_from_directory(
-    VAL_DIR,
-    validation_split=0.2,
-    subset="validation",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
+    with tf.device('GPU:0'):
+        train_ds = tf.keras.utils.image_dataset_from_directory(
+        TRAIN_DIR,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
 
-    for image_batch, labels_batch in train_ds:
-        print(image_batch.shape)
-        print(labels_batch.shape)
-        break
+        val_ds = tf.keras.utils.image_dataset_from_directory(
+        VAL_DIR,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
 
-    AUTOTUNE = tf.data.AUTOTUNE
+        for image_batch, labels_batch in train_ds:
+            print(image_batch.shape)
+            print(labels_batch.shape)
+            break
 
-    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+        AUTOTUNE = tf.data.AUTOTUNE
 
-    num_classes = 9
+        train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+        val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-    model = Sequential([
-    layers.Conv2D(64, kernel_size=5 ,padding='same', activation='relu'),
-    layers.AveragePooling2D(),
-    layers.Conv2D(128, kernel_size=5, padding='same', activation='relu'),
-    layers.AveragePooling2D(),
-    layers.Conv2D(256, padding='same', kernel_size = 2, activation='relu'),
-    layers.AveragePooling2D(),
-    layers.Flatten(),
-    layers.Dense(512, activation='relu'),
-    layers.Dense(256, activation='relu'),
-    layers.Dense(7, activation='linear'),
-    layers.Dense(num_classes)
-    ])
+        num_classes = 9
 
-    model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+        model = Sequential([
+        layers.Conv2D(256, kernel_size=3 ,padding='same', activation='relu'),
+        layers.Conv2D(512, kernel_size=3 ,padding='same', activation='relu'),
+        layers.BatchNormalization(),
+        layers.MaxPooling2D(pool_size = (2,2)),
+        layers.Dropout(0.2),
 
-    # print(model.summary())
+        layers.Conv2D(384, kernel_size=3, padding='same', activation='relu'),
+        layers.BatchNormalization(),
+        layers.MaxPooling2D(pool_size = (2,2)),
+        layers.Dropout(0.2),
 
-    history = model.fit(
-    train_ds,
-    validation_data=val_ds,
-    epochs=epochs
-    )
+
+        layers.Conv2D(192, padding='same', kernel_size = 3, activation='relu'),
+        layers.BatchNormalization(),
+        layers.MaxPooling2D(pool_size = (2,2)),
+        layers.Dropout(0.2),
+
+        layers.Flatten(),
+        layers.Dense(256, activation='relu'),
+        layers.BatchNormalization(),
+        layers.Dropout(0.2),
+        layers.Dense(num_classes, activation = 'softmax', activity_regularizer=tf.keras.regularizers.L2(0.01))
+        ])
+
+        model.compile(optimizer='adam',
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+                metrics=['accuracy'])
+
+        # print(model.summary())
+
+        history = model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=epochs
+        )
+
+
+
+        tf.keras.callbacks.EarlyStopping(
+            monitor='val_loss',
+            min_delta=0,
+            patience=0,
+            verbose=0,
+            mode='auto',
+            baseline=None,
+            restore_best_weights=False
+        )
+
+
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs_range = range(epochs)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
