c5689360bbae74e5a1235e96f993e29325a24ea1
Harry Chih <harryqi99@gmail.com>
2022-05-08 23:03:46 -0400

    Sentiment_CNN_MegaTuning_Tensorflow

--- sentiment_tf.py
+++ sentiment_tf.py
@@ -35,31 +35,6 @@ if __name__ == "__main__":
     MODEL_DIR = "models/"
     LOG_DIR = 'log/'
 
-    # tf.debugging.set_log_device_placement(True)
-
-    # gpus = tf.config.experimental.list_physical_devices('GPU')
-    # if gpus:
-    #     try:
-    #         # Currently, memory growth needs to be the same across GPUs
-    #         for gpu in gpus:
-    #             tf.config.experimental.set_memory_growth(gpu, True)
-    #             logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-    #             print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-    #     except RuntimeError as e:
-    #         # Memory growth must be set before GPUs have been initialized
-    #         print(e)
- 
-    # )
-    # test_gen = test_datagen.flow_from_directory(
-    #     directory=test_dataset_path,
-    #     target_size=(48, 48),
-    #     color_mode="grayscale",
-    #     class_mode="categorical",
-    #     batch_size=batch_size,
-    #     shuffle=True,
-    #     seed=42
-    # )
-
 
     with tf.device('GPU:0'):
         train_datagen = ImageDataGenerator(
@@ -76,7 +51,7 @@ if __name__ == "__main__":
             batch_size=batch_size,
             class_mode="categorical",
             shuffle=True,
-            seed=42
+            seed=55
         )
         test_datagen = ImageDataGenerator(
             rescale=1 / 255.0)
@@ -88,29 +63,9 @@ if __name__ == "__main__":
             class_mode="categorical",
             batch_size=batch_size,
             shuffle=True,
-            seed=42
+            seed=55
         )
 
-        # train_ds = tf.keras.utils.image_dataset_from_directory(
-        # TRAIN_DIR,
-        # validation_split=0.2,
-        # subset="training",
-        # seed=123,
-        # image_size=(img_height, img_width),
-        # batch_size=batch_size)
-
-        # val_ds = tf.keras.utils.image_dataset_from_directory(
-        # VAL_DIR,
-        # validation_split=0.2,
-        # subset="validation",
-        # seed=123,
-        # image_size=(img_height, img_width),
-        # batch_size=batch_size)
-
-        # for image_batch, labels_batch in train_ds:
-        #     print(image_batch.shape)
-        #     print(labels_batch.shape)
-        #     break
 
         # AUTOTUNE = tf.data.AUTOTUNE
 
@@ -143,53 +98,30 @@ if __name__ == "__main__":
         layers.Dense(num_classes, activation = 'softmax', activity_regularizer=tf.keras.regularizers.L2(0.01))
         ])
 
-        # model = base_model.build()
-
         # model.summary()
-        training_weights='./weights'  #这里是保存每次训练权重的  如果需要自己取消注释
+        training_weights='./weights'  #　training weights path
         checkpoint_period = ModelCheckpoint(training_weights + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
                                             monitor='val_loss', save_weights_only=True, save_best_only=False, period=1)
-        #reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=4, verbose=1) #学习率衰减
-        early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1) # val_loss 不下降时 停止训练 防止过拟合
-        tensorboard = TensorBoard(log_dir=LOG_DIR)  #训练日志
+        #reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=4, verbose=1) #learning rate decreasing
+        early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1) # val_loss not descresing, stop trainging to avoid overfitting
+        tensorboard = TensorBoard(log_dir=LOG_DIR)  #training log
         optimizer=tf.keras.optimizers.Adam(learning_rate=lr)
         model.compile(loss=tf.keras.losses.categorical_crossentropy, metrics='acc',optimizer=optimizer)
         history = model.fit(train_generator,validation_data=valid_generator,
                         epochs=epochs,callbacks=[tensorboard, early_stopping,checkpoint_period])
         # model.evaluate(test_gen,verbose=1)
-        model.save(MODEL_DIR+'base_model.h5')
-        # model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-2),
-        #         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-        #         metrics=['accuracy'])
-
-        # print(model.summary())
-
-        # history = model.fit(
-        # train_ds,
-        # validation_data=val_ds,
-        # epochs=epochs
-        # )
+        model.save(MODEL_DIR+'CNN_MegaTuning_model.h5')
 
 
 
-        # tf.keras.callbacks.EarlyStopping(
-        #     monitor='val_loss',
-        #     min_delta=0,
-        #     patience=0,
-        #     verbose=0,
-        #     mode='auto',
-        #     baseline=None,
-        #     restore_best_weights=False
-        # )
-
-
-
-    acc = history.history['accuracy']
-    val_acc = history.history['val_accuracy']
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
 
     loss = history.history['loss']
     val_loss = history.history['val_loss']
-    epochs_range = range(epochs)
+    lr = history.history['lr']
+    print("Learning Rate History: ", lr)
+    epochs_range = [i for i in range(1,len(lr)+1)]
 
     plt.figure(figsize=(8, 8))
     plt.subplot(1, 2, 1)
